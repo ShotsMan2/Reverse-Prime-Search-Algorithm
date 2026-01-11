40e8a66936e974c33bd95456ecd564f134963759
Selim Efe <uysalselimefe@gmail.com>
2026-01-11 11:42:03 +0300

    Implement reverse iterative search for prime identification
    Added source code that iterates downwards from an upper bound ($N-1$) to find the 3rd valid prime number using trial division logic.

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     int sayi,i,asal,j,sum;
+
+    printf("sayi ");
+    scanf("%d",&sayi);
+
+    i=sayi-1;
+
+    for(;i>=2;i--){
+        asal=1;
+        for(j=2;j<=i/2;j++){
+            if(i%j==0){
+                asal=0;
+            }
+        }
+        if(i==2){
+            asal=1;
+        }
+        if(asal==1){
+            sum++;
+        }
+        if(sum==3){
+            printf("sonuc: %d",i);
+            break;
+        }
+    }
+
+
+    return 0;
+}
