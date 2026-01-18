54875f56158a8c5974597c63fba801e19434571a
Selim Efe <uysalselimefe@gmail.com>
2026-01-18 23:09:46 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,32 +3,37 @@
 
 int main()
 {
-     int sayi,i,asal,j,sum;
+    int sayi, i, asal, j, sum;
 
     printf("sayi ");
-    scanf("%d",&sayi);
+    scanf("%d", &sayi);
 
-    i=sayi-1;
+    i = sayi - 1;
 
-    for(;i>=2;i--){
-        asal=1;
-        for(j=2;j<=i/2;j++){
-            if(i%j==0){
-                asal=0;
+    for (; i >= 2; i--)
+    {
+        asal = 1;
+        for (j = 2; j <= i / 2; j++)
+        {
+            if (i % j == 0)
+            {
+                asal = 0;
             }
         }
-        if(i==2){
-            asal=1;
+        if (i == 2)
+        {
+            asal = 1;
         }
-        if(asal==1){
+        if (asal == 1)
+        {
             sum++;
         }
-        if(sum==3){
-            printf("sonuc: %d",i);
+        if (sum == 3)
+        {
+            printf("sonuc: %d", i);
             break;
         }
     }
-
 
     return 0;
 }
